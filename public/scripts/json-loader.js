4fb555420235afdf9627404718cf6c1b133d26e9
Saifullah Jailani <sjailani@berkeley.edu>
2022-06-28 18:44:34 -0700

    most works

--- public/scripts/json-loader.js
+++ public/scripts/json-loader.js
@@ -1,46 +1,103 @@
-[
+"use strict";
+const trails = [
     {
-        "difficulty": "3",
-        "name": "Awesome Trail",
+        "Difficulty": "3",
+        "Name": "Awesome Trail",
         "Duration": "2 hr",
-        "wildlife": "Buffalo, Tiger, Lion",
-        "rating": "1",
-        "distance": "12m",
-        "elevation": "20m"
+        "Wildlife": "Buffalo, Tiger, Lion",
+        "Rating": "1",
+        "Distance": "12m",
+        "Elevation": "20m"
     },
     {
-        "difficulty": "2",
-        "name": "Good Trail",
+        "Difficulty": "2",
+        "Name": "Good Trail",
         "Duration": "3 hr",
-        "wildlife": "Buffalo, Tiger, Lion",
-        "rating": "4",
-        "distance": "100m",
-        "elevation": "23m"
+        "Wildlife": "Buffalo, Tiger, Lion",
+        "Rating": "4",
+        "Distance": "100m",
+        "Elevation": "23m"
     },
     {
-        "difficulty": "1",
-        "name": "Best Trail",
+        "Difficulty": "1",
+        "Name": "Best Trail",
         "Duration": "1 hr",
-        "wildlife": "Buffalo, Tiger, Lion",
-        "rating": "2",
-        "distance": "60m",
-        "elevation": "40m"
+        "Wildlife": "Buffalo, Tiger, Lion",
+        "Rating": "2",
+        "Distance": "60m",
+        "Elevation": "40m"
     },
     {
-        "difficulty": "4",
-        "name": "Wow Trail",
+        "Difficulty": "4",
+        "Name": "Wow Trail",
         "Duration": "0.5 hr",
-        "wildlife": "Buffalo, Tiger, Lion",
-        "rating": "3",
-        "distance": "12m",
-        "elevation": "26m"
+        "Wildlife": "Buffalo, Tiger, Lion",
+        "Rating": "3",
+        "Distance": "12m",
+        "Elevation": "26m"
     }, {
-        "difficulty": "5",
-        "name": "Foo Trail",
+        "Difficulty": "5",
+        "Name": "Foo Trail",
         "Duration": "15 min",
-        "wildlife": "Buffalo, Tiger, Lion",
-        "rating": "5",
-        "distance": "12m",
-        "elevation": "35m"
+        "Wildlife": "Buffalo, Tiger, Lion",
+        "Rating": "5",
+        "Distance": "12m",
+        "Elevation": "35m"
     },
 ];
+
+function print_f(){
+    var key = localStorage.getItem("search");
+    localStorage.removeItem("search");
+    console.log(key);
+    if(key==""){
+        print_f_empty();
+    } else
+    {
+        print_search(key);
+    }
+
+}
+
+function print_f_empty(){
+    
+    var box = document.getElementById("Rating-box");
+    box.innerHTML = `
+    <p class="header-font" id="Rating">
+    Match Not Found
+    </p>
+    `;
+
+}
+document.addEventListener("DOMContentLoaded", print_f());
+
+
+function print_card(elem){
+    console.log("here");
+    var grid = document.getElementById("simple-grid-i");
+    for(var p in elem){
+        var div = document.createElement('div');
+        div.innerHTML = `
+        <p class="simple-grid-font">
+        ${p} : ${elem[p]}
+        </p>
+        `
+        grid.appendChild(div);
+    }
+    
+    
+}
+
+
+function print_search(key){
+    
+    for(var i = 0; i < trails.length; i++){
+        if(trails[i].Name.toLowerCase().search(key.toLowerCase())!=-1 || trails[i].Wildlife.toLowerCase().search(key.toLowerCase()) != -1){
+            print_card(trails[i]);
+            break;
+        }
+        
+    }
+    
+}
+
--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,68 +1,10 @@
 function searchClicked(){
    var search_bar = document.getElementById("search-bar");
-   self.location = "./../search-screen.html";
-   print_f(search_bar.value);
-}
-
-function print_f(name){
-    if(name==""){
-        print_f_empty();
-    }
+   localStorage.setItem("search",search_bar.value);
+   window.document.location = "./../search-screen.html";
 
 }
 
-function print_f_empty(){
-    var box = document.getElementById("rating-box").appendChild(`<p class="header-font">
-    Match Not Found
-  </p>`);
-    console.log("print")
-}
 
-const trails = [
-    {
-        "difficulty": "3",
-        "name": "Awesome Trail",
-        "Duration": "2 hr",
-        "wildlife": "Buffalo, Tiger, Lion",
-        "rating": "1",
-        "distance": "12m",
-        "elevation": "20m"
-    },
-    {
-        "difficulty": "2",
-        "name": "Good Trail",
-        "Duration": "3 hr",
-        "wildlife": "Buffalo, Tiger, Lion",
-        "rating": "4",
-        "distance": "100m",
-        "elevation": "23m"
-    },
-    {
-        "difficulty": "1",
-        "name": "Best Trail",
-        "Duration": "1 hr",
-        "wildlife": "Buffalo, Tiger, Lion",
-        "rating": "2",
-        "distance": "60m",
-        "elevation": "40m"
-    },
-    {
-        "difficulty": "4",
-        "name": "Wow Trail",
-        "Duration": "0.5 hr",
-        "wildlife": "Buffalo, Tiger, Lion",
-        "rating": "3",
-        "distance": "12m",
-        "elevation": "26m"
-    }, {
-        "difficulty": "5",
-        "name": "Foo Trail",
-        "Duration": "15 min",
-        "wildlife": "Buffalo, Tiger, Lion",
-        "rating": "5",
-        "distance": "12m",
-        "elevation": "35m"
-    },
-];
 
 
